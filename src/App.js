23838220bec0a781d86dae0db022e25065f5060e
gmawji <git@gmawji.com>
2017-08-11 04:21:10 -0400

    componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
-// import * as BooksAPI from "./BooksAPI";
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import ListBooks from './components/ListBooks'
 
 class BooksApp extends Component {
   state = {
     books: [],
+
+    // TODO: use url for routing
     showSearchPage: false
   };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+      // Check to make sure we setState properly.
+      console.log(this.state.books.map(books => books))
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <ListBooks/>
+        <ListBooks
+          books={this.state.books}
+        />
       </div>
     )
   }
