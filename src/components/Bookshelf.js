885bf9bf553d57fa794f21e3ddd89133cab773c5
gmawji <git@gmawji.com>
2017-08-13 00:08:18 -0400

    Change to function, .map() props.book for title and author note author is an array called authors

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,27 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import Book from "./Book";
 
-class Bookshelf extends Component {
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-              <Book />
+function Bookshelf(props) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">Currently Reading</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.map(book => (
+            <li key={book.id + book.title}>
+              <Book 
+                book={book}
+              />
             </li>
-          </ol>
-        </div>
-        <div className="open-search">
-          {/* TODO: Change to Link to */}
-          <a onClick={() => this.setState({ showSearchPage: true })}>
-            Add a book
-          </a>
-        </div>
+          ))}
+        </ol>
       </div>
-    );
-  }
+      <div className="open-search">
+        {/* TODO: Change to Link to */}
+        <a onClick={() => this.setState({ showSearchPage: true })}>
+          Add a book
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Bookshelf
