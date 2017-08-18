cca5f401c041c4985113fb4a584df57363cdb397
gmawji <git@gmawji.com>
2017-08-17 21:14:55 -0400

    PropTypes

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import Book from './Book'
 
 class Search extends Component {
+  
+  static PropTypes ={
+    books: PropTypes.array.isRequired,
+    getBookshelf: PropTypes.func.isRequired,
+    changeBookshelf: PropTypes.func.isRequired,
+    searchBooks: PropTypes.func.isRequired
+  }
 
   render() {
+    const {getBookshelf, changeBookshelf, searchBooks, books} = this.props
     return(
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            {/* 
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-              
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author" onChange={this.props.searchBooks}/>
-            
+            <input type="text" placeholder="Search by title or author" onChange={searchBooks}/>
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.props.books.map(book =>
+            {books.map(book =>
               <li key={book.id + book.title}>
                 <Book
                   book={book}
-                  getBookshelf={this.props.getBookshelf}
-                  changeBookshelf={this.props.changeBookshelf}
+                  getBookshelf={getBookshelf}
+                  changeBookshelf={changeBookshelf}
                 />
               </li>
             )}
